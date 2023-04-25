47d07732585f8e3bae29682712a30d7d7246dff2
sohit <sohitkhanchi@gmail.com>
2023-04-25 10:48:51 +0530

    repository implementation

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,75 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreConfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreConfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "64450a2d087d282869f4e309",
+                    Name = "IPhone X",
+                    Category = "Smart Phone",
+                    Summary = "This phone is company's biggest change to flagship smartphones",
+                    Description = "Iphone x 64 Mega Pixel",
+                    ImageFile = "product-1.png",
+                    Price = 655.67M
+                },
+                new Product()
+                {
+                    Id = "64450a2d087d282869f4e310",
+                    Name = "Samsung 10",
+                    Category = "Laptop",
+                    Summary = "This phone is company's biggest change to flagship smartphones",
+                    Description = "Samsung 10 smart phone",
+                    ImageFile = "product-2.png",
+                    Price = 840.25M
+                },
+                new Product()
+                {
+                    Id = "64450a2d087d282869f4e311",
+                    Name = "HTC U11+ Plus",
+                    Category = "Laptop",
+                    Summary = "This phone is company's biggest change to flagship smartphones",
+                    Description = "HTC U11 Plus 256 GB",
+                    ImageFile = "product-3.png",
+                    Price = 640.25M
+                },
+                new Product()
+                {
+                    Id = "64450a2d087d282869f4e312",
+                    Name = "LG G7 ThinQ",
+                    Category = "Home Kitchen",
+                    Summary = "This home kitchen item is company's biggest change to flagship smartphones",
+                    Description = "LG G7 ThinQ",
+                    ImageFile = "product-4.png",
+                    Price = 1640.25M
+                },
+                new Product()
+                {
+                    Id = "64450a2d087d282869f4e313",
+                    Name = "LG Window AC",
+                    Category = "Home",
+                    Summary = "This home AC item is company's biggest change to flagship smartphones",
+                    Description = "LG Window AC",
+                    ImageFile = "product-5.png",
+                    Price = 6640.25M
+                }
+
+            };
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,76 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System.Formats.Asn1;
+using System.Xml.Linq;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _catalogContext;
+
+        public ProductRepository(ICatalogContext catalogContext)
+        {
+            this._catalogContext = catalogContext ?? throw new ArgumentNullException(nameof(catalogContext));
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _catalogContext.Products.
+                Find(p => true)
+                .ToListAsync();
+        }
+
+        public async Task<Product> GetProductAsync(string id)
+        {
+            return await _catalogContext.Products.
+                Find(p => true)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByNameAsync(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+            return await _catalogContext
+                          .Products
+                          .Find(filter)
+                          .ToListAsync();
+        }
+        public async Task<IEnumerable<Product>> GetProductByCategoryAsync(string categoryName)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, categoryName);
+            return await _catalogContext
+                          .Products
+                          .Find(filter)
+                          .ToListAsync();
+        }
+
+
+        public async Task CreateProduct(Product product)
+        {
+            await _catalogContext.Products.InsertOneAsync(product);
+
+        }
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult =
+                            await _catalogContext.Products
+                                    .ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+            return updateResult.IsAcknowledged
+                        && updateResult.ModifiedCount > 0;
+
+        }
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+            var deleteResult =
+                               await _catalogContext.Products
+                                       .DeleteOneAsync(filter);
+            return deleteResult.IsAcknowledged
+                        && deleteResult.DeletedCount > 0;
+        }
+
+
+    }
+}
