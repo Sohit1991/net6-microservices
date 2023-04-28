d3a06fcb08a8122691c0b7ee1908fa98dbd6047d
sohit <sohitkhanchi@gmail.com>
2023-04-29 01:11:53 +0530

    feature/catelog-service: basket api completed

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,44 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            this._repository = repository?? throw new ArgumentNullException(nameof(_repository));
+        }
+
+        [HttpGet("{userName}",Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket=await _repository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName)); // becuase new user might come first time to basket            
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            var basketData = await _repository.UpdateBasket(basket);
+            return Ok(basketData);
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> DeleteBasket(string userName)
+        {
+           await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
