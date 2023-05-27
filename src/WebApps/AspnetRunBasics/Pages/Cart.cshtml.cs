ffc69efb38e02a7dc192bf10320ec002f744de7a
sohit <sohitkhanchi@gmail.com>
2023-05-27 14:37:12 +0530

    feature/Catalog-service : added shopping web application

---
+++ src/WebApps/AspnetRunBasics/Pages/Cart.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics
+{
+    public class CartModel : PageModel
+    {
+        private readonly IBasketService _basketService;
+
+        public CartModel(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        public BasketModel Cart { get; set; } = new BasketModel();        
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Cart = await _basketService.GetBasket("sohit");            
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostRemoveToCartAsync(string productId)
+        {
+            string userName = "sohit";
+            var basket=await _basketService.GetBasket(userName);
+            var item = basket.Items.SingleOrDefault(a => a.ProductId == productId);
+            basket.Items.Remove(item);
+            var basketUpdated=await _basketService.UpdateBasket(basket);
+            return RedirectToPage();
+        }
+    }
+}
---
+++ src/WebApps/AspnetRunBasics/Pages/Product.cshtml.cs
@@ -0,0 +1,67 @@
+﻿using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics
+{
+    public class ProductModel : PageModel
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+
+
+        public ProductModel(ICatalogService catalogService, IBasketService basketService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+        }
+
+        public IEnumerable<string> CategoryList { get; set; } = new List<string>();
+        public IEnumerable<CatalogModel> ProductList { get; set; } = new List<CatalogModel>();
+
+
+        [BindProperty(SupportsGet = true)]
+        public string SelectedCategory { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string? categoryName)
+        {
+            var productList = await _catalogService.GetCatalog();
+            CategoryList = productList.Select(a => a.Category).Distinct();
+            if (!string.IsNullOrEmpty(categoryName))
+            {
+                productList = productList.Where(a => a.Category == categoryName);
+                SelectedCategory = categoryName;
+            }
+            else
+            {
+                ProductList = productList;
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAddToCartAsync(string productId)
+        {
+            //if (!User.Identity.IsAuthenticated)
+            //    return RedirectToPage("./Account/Login", new { area = "Identity" });
+
+            var product = await _catalogService.GetCatalog(productId);
+            var user = "sohit";
+            var basket = await _basketService.GetBasket(user);
+            basket.Items.Add(new BasketItemModel()
+            {
+                ProductId = productId,
+                ProductName = product.Name,
+                Color = "Red",
+                Price = product.Price,
+                Quantity = 1,
+            });
+            await _basketService.UpdateBasket(basket);
+            return RedirectToPage("Cart");
+        }
+    }
+}
